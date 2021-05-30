789c431485f14f4d4713551492838aef0fd02861
JoannaCoPo <jococopo@gmail.com>
2021-05-30 13:17:41 -0600

    Adjust ruby -v and week_1 exs

--- spec/models/aroc_week_1_spec.rb
+++ spec/models/aroc_week_1_spec.rb
@@ -13,12 +13,13 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
 
   it '1. finds orders by amount' do
     # ----------------------- Using Ruby -------------------------
-    orders_of_500 = Order.all.select { |order| order.amount == 500 }
-    orders_of_200 = Order.all.select { |order| order.amount == 200 }
+    # orders_of_500 = Order.all.select { |order| order.amount == 500 }
+    # orders_of_200 = Order.all.select { |order| order.amount == 200 }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+    orders_of_500 = Order.where(amount:500)
+    orders_of_200 = Order.where(amount:200)
     # ------------------------------------------------------------
 
     # Expectation
@@ -28,12 +29,11 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
 
   it '2. finds order id of smallest order' do
     # ----------------------- Using Raw SQL ----------------------
-    order_id = ActiveRecord::Base.connection.execute('SELECT id FROM orders ORDER BY amount ASC LIMIT 1').first['id']
+    # order_id = ActiveRecord::Base.connection.execute('SELECT id FROM orders ORDER BY amount ASC LIMIT 1').first['id']
     # ------------------------------------------------------------
-
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
-    # Your solution should not contain the ID of the order anywhere
+    order_id = Order.select(:id).order(:amount).limit(1).first[:id]
     # ------------------------------------------------------------
 
     # Expectation
@@ -42,11 +42,11 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
 
   it '3. finds order id of largest order' do
     # ----------------------- Using Raw SQL ----------------------
-    order_id = ActiveRecord::Base.connection.execute('SELECT id FROM orders ORDER BY amount DESC LIMIT 1').first['id']
+    # order_id = ActiveRecord::Base.connection.execute('SELECT id FROM orders ORDER BY amount DESC LIMIT 1').first['id']
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+    order_id = Order.select(:id).order(amount: :desc).limit(1)[0][:id]
     # Your solution should not contain the ID of the order anywhere
     # ------------------------------------------------------------
 
